4f9313e926bc7386dac4b8158d83cda90739aeaf
cgcdoss <gabi04061997@gmail.com>
2026-03-22 16:13:25 -0300

    chore: adicionando container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,19 +27,20 @@
     let { data }: PageProps = $props();
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-3">
-    <Geral result={data.result} />
-    <GolsAssistenciasDefesas result={data.result} />
-    <div class="flex flex-col gap-3">
-        <Medias result={data.result} />
-        <Tempojogo result={data.result} />
+<main class="container mx-auto">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+        <Geral result={data.result} />
+        <GolsAssistenciasDefesas result={data.result} />
+        <div class="flex flex-col gap-3">
+            <Medias result={data.result} />
+            <Tempojogo result={data.result} />
+        </div>
+        <div class="grid grid-cols-2 gap-4">
+            <Playerstats result={data.result} player="Gabriel" />
+            <Playerstats result={data.result} player="Michel" />
+            <Playerstats result={data.result} player="Dudu" />
+            <Playerstats result={data.result} player="Leandro" />
+        </div>
+        <Radar result={data.result} />
     </div>
-    <div class="grid grid-cols-2 gap-4">
-        <Playerstats result={data.result} player="Gabriel" />
-        <Playerstats result={data.result} player="Michel" />
-        <Playerstats result={data.result} player="Dudu" />
-        <Playerstats result={data.result} player="Leandro" />
-
-    </div>
-    <Radar result={data.result} />
-</div>
+</main>
